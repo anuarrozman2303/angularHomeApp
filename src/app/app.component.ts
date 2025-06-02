7aea38afc0fb1ccc7151962f9753d64425aeb0e1
Anuar Rozman <anuarrozman2303@gmail.com>
2025-06-03 00:00:16 +0800

    Done Part 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: `<h1>Hello Anuar!</h1>`,
+  // template: `<h1>Anuar Rozman</h1>`,
+  template: `
+    <main>
+      <header class="brand-name">
+        <img class="brand-logo" src="/assets/logo.svg" alt="Logo" aria-hidden="true" />
+      </header>
+      <section class="content">
+        <app-home>
+          <h1> Home App</h1>
+        </app-home>
+      </section>
+    </main>
+  `,
   styleUrls: ['./app.component.css'],
+  imports: [HomeComponent],
 })
 export class AppComponent {
   title = 'homes';
