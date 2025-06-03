7b93c5267755cc68dc3114803b96a3425fca85b3
Anuar Rozman <95499848+anuarrozman2303@users.noreply.github.com>
2025-06-03 10:51:51 +0800

    Forms in Angular
    Create a simple application form

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,23 +1,81 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housing-location';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   template: `
-    <p>
-      details works! {{ housingLocationId }}
-    </p>
+    <!-- <p>
+      details works! {{ housingLocation?.id }}
+    </p> -->
+    <article>
+      <img class="listing-photo" [src]="housingLocation?.photo" alt="">
+      <section class="listing-description">
+        <h2 class="listing-heading">{{housingLocation?.name}}</h2>
+        <p class="listing-location">{{housingLocation?.city}}, {{housingLocation?.state}}</p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this housing location</h2>
+        <ul>
+          <li>
+            Units available: {{housingLocation?.availableUnits}}
+          </li>
+          <li>
+            Does this location have WiFi? {{housingLocation?.wifi ? 'Yes' : 'No'}}  
+          </li>
+          <li>
+            Does this location have laundry? {{housingLocation?.laundry ? 'Yes' : 'No'}}
+          </li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply now to live here</h2>
+        <!-- <button class="primary" type="button">Apply Now</button> -->
+         <form [formGroup]="applyForm" (submit)="submitApplication()">
+          <label for="first-name">First Name</label>
+          <input id="first-name" type="text" formControlName="firstName">
+          <label for="last-name">Last Name</label>
+          <input id="last-name" type="text" formControlName="lastName">
+          <label for="email">Email</label>
+          <input id="email" type="email" formControlName="email">
+
+          <button type="submit" class="primary">Apply Now</button>
+         </form>
+      </section>
+    </article>
   `,
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId: number = 0;
+  // housingLocationId: number = 0;
+  housingService: HousingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
   
+  // Form Model
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  })
+
   constructor() {
-    this.housingLocationId = Number(this.route.snapshot.paramMap.get('id'));
+    // this.housingLocationId = Number(this.route.snapshot.paramMap.get('id'));
+    const housingLocationId = Number(this.route.snapshot.paramMap.get('id'));
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
   }
+
+  submitApplication() {
+    this.housingService.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '', 
+    );
+  }
+  
 }
